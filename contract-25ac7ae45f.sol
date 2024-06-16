424ebd0950a480f5c14a18c67d05e4a05549bc5d
Ilker Ozturk <xentzenith@gmail.com>
2024-06-16 00:29:33 -0400

    Add files via upload

---
+++ contract-25ac7ae45f.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts@5.0.2/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@5.0.2/access/AccessControl.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Permit.sol";
+import "@openzeppelin/contracts@5.0.2/token/ERC20/extensions/ERC20Votes.sol";
+
+/// @custom:security-contact contact@yuvatoken.com
+contract YUVA is ERC20, ERC20Burnable, AccessControl, ERC20Permit, ERC20Votes {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor(address defaultAdmin, address minter)
+        ERC20("YUVA", "YUVA")
+        ERC20Permit("YUVA")
+    {
+        _mint(msg.sender, 202406164 * 10 ** decimals());
+        _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin);
+        _grantRole(MINTER_ROLE, minter);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(to, amount);
+    }
+
+    function clock() public view override returns (uint48) {
+        return uint48(block.timestamp);
+    }
+
+    // solhint-disable-next-line func-name-mixedcase
+    function CLOCK_MODE() public pure override returns (string memory) {
+        return "mode=timestamp";
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _update(address from, address to, uint256 value)
+        internal
+        override(ERC20, ERC20Votes)
+    {
+        super._update(from, to, value);
+    }
+
+    function nonces(address owner)
+        public
+        view
+        override(ERC20Permit, Nonces)
+        returns (uint256)
+    {
+        return super.nonces(owner);
+    }
+}
